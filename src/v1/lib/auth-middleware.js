b6bcad0103a101ba02012c4a75f711f559a289c7
Yu Cao <ycao@redhat.com>
2020-05-21 22:14:39 -0400

    Fix graphiql and allow local debug (#30)

--- src/v1/lib/auth-middleware.js
+++ src/v1/lib/auth-middleware.js
@@ -6,10 +6,11 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+/* Copyright (c) 2020 Red Hat, Inc.
+*/
 
 import _ from 'lodash';
 import lru from 'lru-cache';
-import config from '../../../config';
 import createMockIAMHTTP from '../mocks/iam-http';
 import request from './request';
 
@@ -19,46 +20,6 @@ const asyncMiddleware = fn => (req, res, next) => {
     .catch(next);
 };
 
-// async function getKubeToken({
-//   authorization,
-//   cache,
-//   httpLib,
-//   shouldLocalAuth,
-// }) {
-//   let idToken;
-//   if ((_.isEmpty(authorization) && shouldLocalAuth) || process.env.MOCK === 'true') {
-//     // special case for graphiql to work locally
-//     // do not exchange for idtoken since authorization header is empty
-//     idToken = config.get('localKubeToken') || 'localdev';
-//   } else {
-//     const accessToken = authorization.substring(7);
-//     // We cache the promise to prevent starting the same request multiple times.
-//     let kubeTokenPromise = cache.get(`kubeToken_${accessToken}`);
-//     if (!kubeTokenPromise) {
-//       const options = {
-//         url: `${config.get('cfcRouterUrl')}/idprovider/v1/auth/exchangetoken`,
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         method: 'POST',
-//         json: true,
-//         form: {
-//           access_token: accessToken,
-//         },
-//       };
-//       kubeTokenPromise = httpLib(options);
-//       cache.set(`kubeToken_${accessToken}`, kubeTokenPromise);
-//     }
-
-//     const response = await kubeTokenPromise;
-//     idToken = _.get(response, 'body.id_token');
-//     if (!idToken) {
-//       throw new Error(`Authentication error: ${JSON.stringify(response.body)}`);
-//     }
-//   }
-
-//   return idToken;
-// }
 async function getKubeToken({
   authorization,
   shouldLocalAuth,
@@ -66,8 +27,12 @@ async function getKubeToken({
   let idToken;
   if ((_.isEmpty(authorization) && shouldLocalAuth) || process.env.MOCK === 'true') {
     // special case for graphiql to work locally
-    // do not exchange for idtoken since authorization header is empty
-    idToken = config.get('localKubeToken') || 'localdev';
+    // use SERVICEACCT_TOKEN to get idtoken
+    if (process.env.SERVICEACCT_TOKEN) {
+      idToken = `Bearer ${process.env.SERVICEACCT_TOKEN}`;
+    } else {
+      idToken = 'localdev';
+    }
   } else {
     idToken = authorization;
     if (!idToken) {
@@ -96,17 +61,6 @@ async function getNamespaces(usertoken) {
   return request(options);
 }
 
-// async function getAccountData({ iamToken, user }) {
-//   const options = { iamToken };
-//   if (process.env.NODE_ENV === 'test') {
-//     options.httpLib = createMockIAMHTTP();
-//   }
-
-//   const idConnector = new IDConnector(options);
-
-//   return idConnector.get(`/identity/api/v1/users/${user}`);
-// }
-
 export default function createAuthMiddleWare({
   cache = lru({
     max: 1000,
@@ -121,57 +75,14 @@ export default function createAuthMiddleWare({
     });
     req.kubeToken = idToken;
 
-    // special case for redhat openshift, can't get user from idtoken
-    // if (!userName) {
-    //   // We cache the promise to prevent starting the same request multiple times.
-    //   let userInfoPromise = cache.get(`userInfo_${iamToken}`);
-    //   if (!userInfoPromise) {
-    //     const options = {
-    //       url: `${config.get('cfcRouterUrl')}/idprovider/v1/auth/userinfo`,
-    //       headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded',
-    //       },
-    //       method: 'POST',
-    //       json: true,
-    //       form: {
-    //         access_token: iamToken,
-    //       },
-    //     };
-    //     userInfoPromise = httpLib(options);
-    //     cache.set(`userInfo_${iamToken}`, userInfoPromise);
-    //   }
-
-    //   const response = await userInfoPromise;
-    //   userName = _.get(response, 'body.sub') || _.get(response, 'body.name');
-    //   if (!userName) {
-    //     throw new Error(`Authentication error: ${response.body}`);
-    //   }
-    // }
-
-    // Get the namespaces for the user.
-    // We cache the promise to prevent starting the same request multiple times.
     let nsPromise = cache.get(`namespaces_${idToken}`);
     if (!nsPromise) {
       nsPromise = getNamespaces(idToken);
       cache.set(`namespaces_${idToken}`, nsPromise);
     }
 
-    // Get user's account data.
-    // We cache the promise to prevent starting the same request multiple times.
-    // let accountPromise = cache.get(`account_${iamToken}`);
-    // if (!accountPromise) {
-    //   accountPromise = getAccountData({
-    //     iamToken,
-    //     user: userName,
-    //   });
-    //   cache.set(`account_${iamToken}`, accountPromise);
-    // }
-
     req.user = {
-      // name: userName,
       namespaces: await nsPromise,
-      // userAccount: await accountPromise,
-      // iamToken,
     };
 
     next();

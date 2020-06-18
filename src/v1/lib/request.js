069ce353df2a19a463507b0dbcfaad0cf4c911f4
Yu Cao <ycao@redhat.com>
2020-06-17 20:43:48 -0400

    retry request when failed due to network issue (#38)

--- src/v1/lib/request.js
+++ src/v1/lib/request.js
@@ -6,7 +6,13 @@
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  ****************************************************************************** */
+/* Copyright (c) 2020 Red Hat, Inc. */
 
-const request = require('requestretry').defaults({ json: true, maxAttempts: 1, strictSSL: false });
+const request = require('requestretry').defaults({
+  json: true,
+  maxAttempts: 5,
+  retryDelay: 500,
+  strictSSL: false,
+});
 
 export default request;
